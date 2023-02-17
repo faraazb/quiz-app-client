e2a93142a6e11b66837a404e3328edca00641956
Vaibhav Patil <vaibhav.patil@contentstack.com>
2023-02-17 20:48:27 +0530

    [t10] removed question component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { Header } from "./components";
 import "./App.css";
-import Question from "./components/Question";
 
 function App() {
     return (
         <div className="app">
             <Header />
-            <main>
-                <Question />
-            </main>
+            <main></main>
         </div>
     );
 }

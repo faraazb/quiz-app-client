47dd5d2562a7d1e40c3ff01f7ff13773a6985664
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2023-02-18 18:07:02 +0530

    [t25] Add routing Add Link to create quiz page in Dashboard

--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import ReactDOM from "react-dom/client";
 import 'antd/dist/reset.css';
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { DashboardPage, StartQuizPage } from "./components";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/dashboard",
+                element: <DashboardPage />
+            },
+            {
+                path: "/quiz/create"
+            },
+            {
+                path: "/quiz/:quizId",
+                element: <StartQuizPage />
+            },
+            {
+                path: "/quiz/:quizId/submissions"
+            },
+            {
+                path: "/quiz/:quizId/submissions/:submissionId"
+            }
+        ]
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );

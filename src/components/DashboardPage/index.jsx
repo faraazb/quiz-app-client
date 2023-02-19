3dfa9bca3318969888d747c824bc6c54006e8276
Kashmeera <Kashmeera.raychoty@contentstack.com>
2023-02-19 17:19:17 +0530

    Made changes in the dashboard

--- src/components/DashboardPage/index.jsx
+++ src/components/DashboardPage/index.jsx
@@ -1,37 +1,39 @@
 import "./dashboardPage.css";
 import { Card, Col, Row } from 'antd';
+import axios from "axios";
 import { Button } from 'antd';
+import { useEffect, useState } from "react";
+// import { Question } from "../../../../quiz-app-server/src/models";
 const DashboardPage = () => {
+    const [quizzes, setQuizzes] = useState(null);
+    const getQuizzes = async () => {
+        try{
+            const quizzes = await axios.get("http://localhost:5000/quizzes/");
+            setQuizzes(quizzes);
+            console.log(quizzes.data.data);
+        }
+        catch(err)  {
+            // console.log(err);
+        }
+    };
+    useEffect(() => {
+        getQuizzes();
+    }, []);
     return  (
         <div className='dashboard'>
             <Button type="primary" href="#" shape='round'>Create Quiz</Button>
             <hr />
-            <Row gutter={[10,10]} justify="center">
-                <Col>
-                    <Card size="small" title="Quiz Title" style={{ width: 200, height: 100 }}>
-                        <p>15 Submissions</p>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card size="small" title="Quiz Title" style={{ width: 200, height: 100 }}>
-                        <p>15 Submissions</p>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card size="small" title="Quiz Title" style={{ width: 200, height: 100 }}>
-                        <p>15 Submissions</p>
-                    </Card>
-                </Col><Col>
-                    <Card size="small" title="Quiz Title" style={{ width: 200, height: 100 }}>
-                        <p>15 Submissions</p>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card size="small" title="Quiz Title" style={{ width: 200, height: 100 }}>
-                        <p>15 Submissions</p>
-                    </Card>
-                </Col>
-            </Row>
+            {quizzes.data.data.map((quiz) => {
+                return (
+                    <Row gutter={[10,10]} justify="center">
+                        <Col>
+                            <Card size="small" title={quiz.title} style={{ width: 200, height: 100 }}>
+                                <p>{quiz.submissionsCount} Submissions</p>
+                            </Card>
+                        </Col>
+                    </Row>
+                )
+            })} 
         </div>
     )
 }

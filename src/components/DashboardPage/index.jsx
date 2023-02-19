c5bfb3dbd9d5ed3d0096c1ac2acb360263172fe1
Kashmeera <Kashmeera.raychoty@contentstack.com>
2023-02-19 19:32:55 +0530

    [t8] Made changes in the dashboard

--- src/components/DashboardPage/index.jsx
+++ src/components/DashboardPage/index.jsx
@@ -3,17 +3,16 @@ import { Card, Col, Row } from 'antd';
 import axios from "axios";
 import { Button } from 'antd';
 import { useEffect, useState } from "react";
-// import { Question } from "../../../../quiz-app-server/src/models";
 const DashboardPage = () => {
     const [quizzes, setQuizzes] = useState(null);
     const getQuizzes = async () => {
         try{
             const quizzes = await axios.get("http://localhost:5000/quizzes/");
             setQuizzes(quizzes);
-            console.log(quizzes.data.data);
+            // console.log(quizzes.data.data);
         }
         catch(err)  {
-            // console.log(err);
+            console.log(err);
         }
     };
     useEffect(() => {
@@ -26,11 +25,11 @@ const DashboardPage = () => {
             <Row gutter={[10,10]} justify="center">
                 {quizzes !== null && quizzes.data.data.map((quiz) => {
                     return (
-                            <Col key = {quiz.id}>
-                                <Card size="small" title={quiz.title} style={{ width: 200, height: 100 }}>
-                                    <p>{quiz.submissionsCount} Submissions</p>
-                                </Card>
-                            </Col>
+                        <Col key = {quiz.id}>
+                            <Card size="small" title={quiz.title} style={{ width: 200, height: 100 }}>
+                                <p>{quiz.submissionsCount} Submissions</p>
+                            </Card>
+                        </Col>
                     )
                 })}
             </Row>

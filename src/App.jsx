b61440c98458e7cfaf457e4d7ddf427ee7a9897e
harshithad0703 <104908717+harshithad0703@users.noreply.github.com>
2023-02-24 12:35:12 +0530

    [t9] removed QuizProvider from app.jsx
    [t9] removed QuizProvider from App.jsx which was to be added in index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { Header, QuizCreationPage } from "./components";
+import { Header } from "./components";
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import { QuizProvider } from "./contexts/CreateQuizContexts";
+
 function App() {
     return (
         <div className="app">
             <Header />
             <main>
                 <Outlet />
-                <QuizProvider>
-                    <QuizCreationPage></QuizCreationPage>
-                </QuizProvider>
             </main>
         </div>
     );

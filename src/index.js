df54a7be6c3878f5d4e35937f95260e839823945
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2023-02-27 05:44:28 +0530

    Add favicon and logo in header

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ const router = createBrowserRouter([
         element: <App />,
         children: [
             {
+                path: "/",
+                element: <DashboardPage />,
+            },
+            {
                 path: "/dashboard",
                 element: <DashboardPage />,
             },
